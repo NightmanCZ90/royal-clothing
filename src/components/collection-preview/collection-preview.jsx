ac2072ce1e1c08dec5a86872cf1983ef80b217c7
NightmanCZ90 <novak.jan90@gmail.com>
2020-12-22 22:43:55 +0100

    created new additem to cart reducer and implemented it to collection item

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -10,9 +10,9 @@ const CollectionPreview = ({ title, items}) => {
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
         {
-          items.filter((item, idx) => idx < 4).map(({ id, ...itemProps }) => {
+          items.filter((item, idx) => idx < 4).map((item) => {
             return (
-              <CollectionItem key={id} {...itemProps} />
+              <CollectionItem key={item.id} item={item} />
             )
           })
         }
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { CartActionTypes } from './cart.types';
 export const toggleCartHidden = () => ({ //payload is optional property and we do not need it in this case
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+})
