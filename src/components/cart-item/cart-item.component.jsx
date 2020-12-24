ea141fc1bbf7c9c9ba14840c4c3b84a55d5400e4
NightmanCZ90 <novak.jan90@gmail.com>
2020-12-24 01:27:51 +0100

    tweaked styles of images

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,9 @@ import './cart-item.styles.scss';
 const CartItem = ({ item: { imageUrl, price, name, quantity }}) => {
   return (
     <div className='cart-item'>
-      <img src={imageUrl} alt='item' />
+      <div className='item-image'>
+        <img src={imageUrl} alt='item-image' />
+      </div>
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">{quantity} x ${price}</span>
