a27f6065ebf94a0f8cc75cbfec39565304c69d29
NightmanCZ90 <novak.jan90@gmail.com>
2020-12-27 19:44:35 +0100

    added addItem and removeItem functionality to checkoutItem component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -45,6 +45,6 @@ const CheckoutPage = ({ cartItems, total }) => {
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal
-})
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,5 @@
-import { clearItemFromCart } from './cart.actions';
 import { CartActionTypes } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -19,6 +18,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload) //from cart.utils
+      }
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload) //also from cart.utils
       }
 
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
