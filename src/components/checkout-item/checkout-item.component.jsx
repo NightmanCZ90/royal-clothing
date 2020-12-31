d0052f36fb255a55db64fbbbedba2131b6c896b8
NightmanCZ90 <novak.jan90@gmail.com>
2020-12-31 01:50:00 +0100

    removed redundant text in alt

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,7 +11,7 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   return (
     <div className='checkout-item'>
       <div className="image-container">
-        <img src={imageUrl} alt='item-image' />
+        <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>
